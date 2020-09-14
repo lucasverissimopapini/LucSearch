bf9072685f0770ff04f1c88ed818db87cfe5c1b8
Lucas Verissimo <lucas.verissimo.souza@hotmail.com>
2020-09-14 12:19:02 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/lucpesquisa/dspesquisa/LucpesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.lucpesquisa.dspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucpesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucpesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lucpesquisa/dspesquisa/LucpesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucpesquisa.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucpesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
