dc48249883e36a9d744c6d1ba30a52b11bc0edb4
Lucas Verissimo <lucas.verissimo.souza@hotmail.com>
2020-09-17 10:16:42 -0300

    Criando página de listagem dos registros

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Route } from 'react-router-dom';
 import './App.css';
 import Routes from './Routes';
 
 function App() {
   return (
-    <Routes/>
-  )    
+    <Routes />
+  )
 }
 
 export default App;
