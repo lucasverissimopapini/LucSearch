f979856dfcc62ed998c60f97401e0a1dffb36f49
Lucas Verissimo <lucas.verissimo.souza@hotmail.com>
2020-09-17 18:14:17 -0300

    Adicionando a pagina de gráficos

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Switch , Route} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from './components/Header';
+import Charts from './pages/Charts';
 import Home from './pages/Home';
 import Records from './pages/Records';
 
 const Routes = () => (
     <BrowserRouter>
-    <Header/>
-    <Switch>
-        <Route path="/" exact>
-            <Home/>
-        </Route>
-        <Route path="/records" exact>
-            <Records/>
-        </Route>
-    </Switch>
+        <Header />
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route path="/records" exact>
+                <Records />
+            </Route>
+            <Route path="/charts" exact>
+                <Charts />
+            </Route>
+        </Switch>
     </BrowserRouter>
 )
 
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Filters from '../../components/Filters';
 import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import './styles.css';
@@ -23,13 +24,7 @@ const Records = () => {
 
     return (
         <div className="page-container">
-            <div className="filters-container records-actions">
-                <Link to="/charts">
-                    <button className="action-filters">
-                        VER GRÁFICOS
-                    </button>
-                </Link>
-            </div>
+            <Filters link="/charts" linkText="VER GRÁFICO" />
             <table className="records-table" cellPadding="0" cellSpacing="0">
                 <thead>
                     <tr>
